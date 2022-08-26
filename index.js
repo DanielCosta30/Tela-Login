be5ee0077baef4e95c0677c1900bb594fee638a5
DanielCosta30 <nielcosta24@gmail.com>
2022-08-26 18:32:00 -0300

    feat(if.else): add propriedades de validação login

--- index.js
+++ index.js
@@ -1,51 +1,69 @@
-
 /* Visualizar senha */
 
-var btn = document.querySelector('.fa-eye')
+var btn = document.querySelector(".fa-eye");
 
-btn.addEventListener('click',()=>{
-    var inputSenha = document.querySelector('#senha')
+btn.addEventListener("click", () => {
+  var inputSenha = document.querySelector("#senha");
 
-if(inputSenha.getAttribute('type') == 'password'){
-    inputSenha.setAttribute('type', 'text')
-} else {
-    inputSenha.setAttribute('type', 'password')
+  if (inputSenha.getAttribute("type") == "password") {
+    inputSenha.setAttribute("type", "text");
+  } else {
+    inputSenha.setAttribute("type", "password");
   }
-})
+});
 
 /* <--------- */
 
 /* acessar login */
 
+var entralogin = document.querySelector(".acesso");
 
-function entrar(){
-var email = document.querySelector('#email')
-var emaillabel = document.querySelector('#emaillabel')
+entralogin.addEventListener("click", function (event) {
+  event.preventDefault();
 
-var senha = document.querySelector ('#senha')
-var senhalabel = document.querySelector('#senhalabel')
+  var email = document.querySelector("#email");
+  var emaillabel = document.querySelector("#emaillabel");
 
-var msgError = document.querySelector('#msgError')
-var listauser = []
+  var senha = document.querySelector("#senha");
+  var senhalabel = document.querySelector("#senhalabel");
 
-var uservalid = {
-    email:'',
-    senha:''
-}
-    listauser = JSON.parse(localStorage.getItem('listauser'))
+  var msgError = document.querySelector("#msgError");
+  var listauser = [];
 
-    /*ForEach  vai vazer minha lsita item por item  em cada lista*/
+  var uservalid = {
+    email: "",
+    senha: "",
+  };
+  listauser = JSON.parse(localStorage.getItem("listauser"));
 
-    listauser.forEach((item) =>{    
-        if (email.value == item.emailcad || senha.value == item.senhacad){
-            uservalid={
-                email: item.emailcad,
-                senha: item.senhacad
-            }
-            
-        }
-  }) 
+  /*ForEach  vai vazer minha lsita item por item  em cada lista*/
 
-}
+  listauser.forEach((item) => {
+    if (email.value == item.emailcad || senha.value == item.senhacad) {
+      uservalid = {
+        email: item.emailcad,
+        senha: item.senhacad,
+      };
+    }
+  });
+
+  if (email.value == uservalid.email && senha.value == uservalid.senha) {
+
+    window.location.href='file:///C:/Users/Daniel%20Costa/Desktop/MENTORIA/Tela_Login/inicio.html'
+    
+  } else {
+
+    emaillabel.setAttribute('style', 'color: red')
+    email.setAttribute('style', 'border-color: red')
+    senhalabel.setAttribute('style', 'color: red')
+    senha.setAttribute('style', 'border-color: red')
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = 'Usuário ou senha incorretos'
+    email.focus()
+
+  }
+
+});
+
 
 /* <--------- */
