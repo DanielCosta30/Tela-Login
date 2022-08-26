989ad99554501768450c1d631854cc06413d1b21
DanielCosta30 <nielcosta24@gmail.com>
2022-08-26 17:23:34 -0300

    feat(uservalid)= validando inf. login

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-let btn = document.querySelector('.fa-eye')
+
+/* Visualizar senha */
+
+var btn = document.querySelector('.fa-eye')
 
 btn.addEventListener('click',()=>{
-    let inputSenha = document.querySelector('#senha')
+    var inputSenha = document.querySelector('#senha')
 
 if(inputSenha.getAttribute('type') == 'password'){
     inputSenha.setAttribute('type', 'text')
 } else {
     inputSenha.setAttribute('type', 'password')
-    
+  }
 })
+
+/* <--------- */
+
+/* acessar login */
+
+
+function entrar(){
+var email = document.querySelector('#email')
+var emaillabel = document.querySelector('#emaillabel')
+
+var senha = document.querySelector ('#senha')
+var senhalabel = document.querySelector('#senhalabel')
+
+var msgError = document.querySelector('#msgError')
+var listauser = []
+
+var uservalid = {
+    email:'',
+    senha:''
+}
+
+}
+
+/* <--------- */
